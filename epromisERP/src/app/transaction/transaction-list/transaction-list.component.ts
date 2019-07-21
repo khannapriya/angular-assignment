ef69e086e79cf0ebd400315c96c2d4c60f113e12
priyakhanna <priyakh87@gmail.com>
2019-07-21 10:07:09 +0400

    accounts and data table changes

--- epromisERP/src/app/transaction/transaction-list/transaction-list.component.ts
+++ epromisERP/src/app/transaction/transaction-list/transaction-list.component.ts
@@ -6,19 +6,19 @@ export interface TransactionList {
   AccountCode: string;
   AccountName: string;
   DefaultCurrency: number;
-  Address: string;
+  // Address: string;
 }
 
 const ELEMENT_DATA: TransactionList[] = [
-  {AccountCode: '12366545487', AccountName: 'Prakesh Iyer', DefaultCurrency: 1.0079, Address: 'Dubai , Deira city center'},
+  {AccountCode: '12366545487', AccountName: 'Prakesh Iyer', DefaultCurrency: 1.0079},
   
-  {AccountCode: '15626545487', AccountName: 'Rohit Sharma', DefaultCurrency: 122.0079, Address: 'Ajman, UAE'},
+  {AccountCode: '15626545487', AccountName: 'Rohit Sharma', DefaultCurrency: 122.0079},
   
-  { AccountCode: '12366545511', AccountName: 'Mahmood Mufti', DefaultCurrency: 141.0079, Address: 'Abu Dabhi, UAE'},
-  { AccountCode: '12366545419', AccountName: 'Arib Mohmaad', DefaultCurrency: 141.0079, Address: 'Abu Dabhi, UAE'},
-  { AccountCode: '12366545480', AccountName: 'Yusuf Mufti', DefaultCurrency: 241.0079, Address: 'Bur Dubai, Dubai, UAE'},
-  { AccountCode: '12366545471', AccountName: 'Shilpy Bindra', DefaultCurrency: 41.0079, Address: 'Abu Dabhi'},
-  { AccountCode: '12366545009', AccountName: 'Ruchika Dhawan', DefaultCurrency:541.0079, Address: 'Al Barsh South,Dubai'},
+  { AccountCode: '12366545511', AccountName: 'Mahmood Mufti', DefaultCurrency: 141.0079},
+  { AccountCode: '12366545419', AccountName: 'Arib Mohmaad', DefaultCurrency: 141.0079},
+  { AccountCode: '12366545480', AccountName: 'Yusuf Mufti', DefaultCurrency: 241.0079},
+  { AccountCode: '12366545471', AccountName: 'Shilpy Bindra', DefaultCurrency: 41.0079},
+  { AccountCode: '12366545009', AccountName: 'Ruchika Dhawan', DefaultCurrency:541.0079},
   
 ];
 
@@ -31,7 +31,7 @@ export class TransactionListComponent implements OnInit {
 
   constructor() { }
 
-  displayedColumns: string[] = ['AccountCode', 'AccountName', 'DefaultCurrency', 'Address'];
+  displayedColumns: string[] = ['AccountCode', 'AccountName', 'DefaultCurrency'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
